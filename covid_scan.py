36d164667235e44fa25692195ee6b8114c564173
Maxwell Herron <Maxwell.herron@fjorgedigital.com>
2020-03-27 15:05:11 -0500

    Began basic file writing

--- covid_scan.py
+++ covid_scan.py
@@ -6,13 +6,12 @@ Created on Thu Mar 26 20:28:08 2020
 @author: maxwell
 """
 
-import scrapy as sp
 import praw
 import datetime
 import os
 import requests
 import config
-import pandas
+import csv
 
 def bot_login():
     """
@@ -24,13 +23,11 @@ def bot_login():
         DESCRIPTION.
 
     """
-    print("Loggin in...")
     bot = praw.Reddit(username = config.username,
                     password = config.password,
                     client_id = config.client_id,
                     client_secret = config.client_secret,
                     user_agent = "My COVID-19 mention scanning bot")
-    print("Logged in!")
     return bot
 
 
@@ -50,23 +47,57 @@ def run_bot(bot):
     """
     subreddits = ("worldnews", "news", "funny", "gaming", "pics", "science", 
                   "videos", "AskReddit", "aww", "askscience", "Tinder")
-    
+    output = {
+        "worldnews" : 0,
+        "news" : 0, 
+        "funny" : 0, 
+        "gaming" : 0, 
+        "pics" : 0, 
+        "science" : 0,                  
+        "videos" : 0, 
+        "AskReddit" : 0, 
+        "aww" : 0, 
+        "askscience" : 0, 
+        "Tinder" : 0
+        }
     print("*"*80)
-    print("Running COVID-19 mention scan")
+    print(" "*10 + "Running Coronavirus mention scan")
     print("*"*80+"\n")
-    
-    for subreddit in subreddits:
-        print("-"*80)
-        print("Scanning r/" + subreddit)
-        print("-"*80)
+    print("-"*80)
+    for subreddit in subreddits:       
+        print("Scanning r/" + subreddit + "\n")
+        count = 0
         current = bot.subreddit(subreddit)
         cutoff_time = datetime.date.today() - datetime.timedelta(1)
         cutoff_time = float(cutoff_time.strftime("%s"))
-        for submission in current.hot():
+        for submission in current.new():
             if submission.created_utc > cutoff_time:
                 current_title = submission.title.lower()
                 if "coronavirus" in current_title:
+                    count += 1
                     print(submission.title + "\n")
+        output[subreddit] = count
+        print("Total mentions of Coronavirus in r/" + subreddit + ":", count)
+        print("-"*80)
+    write_output(output)
+        
+        
+def write_output(output):
+    """
+    
+
+    Parameters
+    ----------
+    output : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    with open("results.csv", 'a') as f:
+        
             
 
 
