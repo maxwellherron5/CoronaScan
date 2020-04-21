b32be37e6b99a53d18073633941f48a237e453ec
Maxwell Herron <Maxwell.herron@fjorgedigital.com>
2020-04-21 13:35:25 -0500

    updated gitignore

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/Users/maxwell/opt/anaconda3/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 20:41:32 2020
+
+@author: maxwell
+"""
+
+username = "Binary_Bot10"
+password = "binaryiscool101"
+client_id = "DFodr3Lxdp1S0g"
+client_secret = "mUVgfenr9K5_T9DmNUtbnqQmKjI"
