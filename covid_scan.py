ab7e5759cd5699e62af6ee987a19fff96978e17e
Maxwell Herron <maxwellherron5@gmail.com>
2020-07-09 00:00:00 -0500

    Adding daily data and plots

--- covid_scan.py
+++ covid_scan.py
@@ -96,7 +96,7 @@ def get_offset():
     to generate the plot from.
     """
     offset = datetime.date.today() - start_day
-    return int(offset.days) - 3
+    return int(offset.days) - 4
 
 
 def generate_day_comparison():
